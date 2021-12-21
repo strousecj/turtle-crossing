da3e2549dfff5f5304bdea89446b99022f7dad46
strousecj <chandler.strouse@gmail.com>
2021-12-21 18:41:44 -0500

    Change yellow car color to gold for better contrast on white background

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 import random
 
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+COLORS = ["red", "orange", "gold", "green", "blue", "purple"]
 MOVE_DISTANCE = 5
 MOVE_INCREMENT = 5
 
